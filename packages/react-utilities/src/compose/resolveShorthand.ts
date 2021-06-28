3184864a3440788e1db8724912469caff1b68834
Bernardo Sunderhus <bsunderhus@microsoft.com>
2021-06-28 18:31:00 +0200

    Updates API

--- packages/react-utilities/src/compose/resolveShorthand.ts
+++ packages/react-utilities/src/compose/resolveShorthand.ts
@@ -4,12 +4,6 @@ import { ObjectShorthandProps, ShorthandProps } from './types';
 /**
  * Resolves ShorthandProps into ObjectShorthandProps, to ensure normalization of the signature
  * being passed down to getSlots method
- *
- * @example
- * const state = {
- *  icon: resolveShorthand(props.icon)
- * }
- *
  * @param value - the base ShorthandProps
  * @param defaultProps - base properties to be merged with the end ObjectShorthandProps
  */
