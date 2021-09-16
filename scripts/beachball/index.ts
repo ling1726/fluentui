61721eac6d82a0890417b1fd6ce98636134dcbc4
Lingfan Gao <lingfangao@hotmail.com>
2021-09-16 17:26:35 +0200

    add .npmrc for test bench

--- scripts/beachball/index.ts
+++ scripts/beachball/index.ts
@@ -1,5 +1,7 @@
 import { BeachballConfig } from 'beachball';
 import { getAllPackageInfo } from '../monorepo/index';
+import { writeFile } from 'fs-extra';
+const path = require('path');
 // import { renderHeader, renderEntry } from './customRenderers';
 
 const allPackageInfo = getAllPackageInfo();
@@ -28,6 +30,24 @@ export const config: BeachballConfig = {
     'https://uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test/npm/registry/',
   // @ts-ignore
   scope: process.env.RELEASE_VNEXT ? fluentConvergedPackagePaths : defaultScope,
+  hooks: {
+    prepublish: packagePath => {
+      const authToken = process.env.ADO_TOKEN;
+      return writeFile(
+        path.join(packagePath, '.npmrc'),
+        `
+; begin auth token
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/registry/:username=uifabric
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/registry/:_password=${authToken}
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/registry/:email=npm requires email to be set but doesn't use the value
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/:username=uifabric
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/:_password=${authToken}
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test1/npm/:email=npm requires email to be set but doesn't use the value
+; end auth token
+      `,
+      );
+    },
+  },
   changelog: {
     // customRenderers: {
     //   renderHeader,
