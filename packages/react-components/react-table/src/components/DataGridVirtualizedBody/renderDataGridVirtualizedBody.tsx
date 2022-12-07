3cb3c49f86e4548900dc36246393a282bd550ca6
Mitch-At-Work <mifraser@microsoft.com>
2022-12-07 10:17:32 -0800

    Let's increase the buffer size

--- packages/react-components/react-table/src/components/DataGridVirtualizedBody/renderDataGridVirtualizedBody.tsx
+++ packages/react-components/react-table/src/components/DataGridVirtualizedBody/renderDataGridVirtualizedBody.tsx
@@ -11,7 +11,7 @@ export const renderDataGridVirtualizedBody_unstable = (state: DataGridVirtualize
   const { slots, slotProps } = getSlots<DataGridBodySlots>(state);
   return (
     <slots.root {...slotProps.root}>
-      <Virtualizer virtualizerLength={70} bufferSize={900} itemSize={state.itemSize}>
+      <Virtualizer virtualizerLength={80} bufferSize={800} bufferItems={20} itemSize={state.itemSize}>
         {slotProps.root.children}
       </Virtualizer>
     </slots.root>
